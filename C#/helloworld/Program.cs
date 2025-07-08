dda73490aa537919a05a0f2996307f3cdfac5bb8
Anish Baniya <anishbaniya07@gmail.com>
2025-07-08 13:31:45 +0545

    learn: string and int

--- C#/helloworld/Program.cs
+++ C#/helloworld/Program.cs
@@ -4,4 +4,16 @@ Console.WriteLine("Hello, World!");
 string firstfriend = "  Maria  ";
 firstfriend = firstfriend.Trim();
 string secondfriend = "Sage";
-Console.WriteLine($"My friends are {firstfriend} and {secondfriend}");
+string friends = $"My friends are {firstfriend} and {secondfriend}";
+//replace example
+//this change is limited to inline ony i.e.
+//if i reprint friends, then old value will be used
+//for fix change you need define explicitly
+Console.WriteLine(friends.Replace("Sage", "Hary"));
+Console.WriteLine(friends.Contains("Sage"));
+
+// Integer
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
