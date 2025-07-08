d4863e1566a4e94b916b8411f20e643bcaa27b01
Anish Baniya <anishbaniya07@gmail.com>
2025-07-08 12:29:23 +0545

    backend learning start

---
+++ C#/helloworld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//string and trim function
+string firstfriend = "  Maria  ";
+firstfriend = firstfriend.Trim();
+string secondfriend = "Sage";
+Console.WriteLine($"My friends are {firstfriend} and {secondfriend}");
