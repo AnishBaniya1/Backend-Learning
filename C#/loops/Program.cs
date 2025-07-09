54ea663b5d2dd80c4ef318c436bfb08af865e25a
Anish Baniya <anishbaniya07@gmail.com>
2025-07-09 18:59:12 +0545

    learn: if condition

---
+++ C#/loops/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//if condition
+int a = 5;
+int b = 4;
+// if (a + b > 10)
+//     Console.WriteLine("Answer is greater than 10.");
+int c = a + b;
+bool myTest = c > 10;
+if (myTest)
+{
+    Console.WriteLine("Answer is greater than 10.");
+}
+else
+{
+    Console.WriteLine("Answer is less than 10.");
+}
