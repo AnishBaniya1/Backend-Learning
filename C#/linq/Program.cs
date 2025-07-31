06b7ed2c133f49d652bea6d7aadc14f9c95e1ce6
Anish Baniya <anishbaniya07@gmail.com>
2025-07-31 15:24:21 +0545

    learn: linq

---
+++ C#/linq/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> scores = [97, 92, 81, 60];
+
+IEnumerable<int> scoreQuery =
+from score in scores where score > 80 select score;
+
+foreach (int i in scoreQuery)
+{
+    Console.Write(i + " ");
+}
