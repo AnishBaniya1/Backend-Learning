0858dc7e2faddbf04309f940729b6d52a01ca21c
Anish Baniya <anishbaniya07@gmail.com>
2025-08-18 17:47:32 +0545

    learn: add swagger to project

--- ASP.NET CORE/DiaryApp/Program.cs
+++ ASP.NET CORE/DiaryApp/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DiaryApp API", Description = "Reminding task", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -14,6 +21,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiaryApp API V1");
+    });
+}
 app.UseRouting();
 
 app.UseAuthorization();
