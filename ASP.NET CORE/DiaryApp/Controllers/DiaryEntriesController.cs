c2a340c3b4c91a2220953aeb6f389d1fd03b0bf1
Anish Baniya <anishbaniya07@gmail.com>
2025-08-23 20:15:18 +0545

    create: view site and add DI, get data for view

--- ASP.NET CORE/DiaryApp/Controllers/DiaryEntriesController.cs
+++ ASP.NET CORE/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,13 +1,25 @@
+using DiaryApp.Data;
+using DiaryApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
+
     public class DiaryEntriesController : Controller
     {
+        //this is dependency injection
+        private readonly ApplicationDbContext _db;
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: DiaryEntriesController
         public ActionResult Index()
         {
-            return View();
+            //to get data
+            List<DiaryEntry> entries = _db.DiaryEntries.ToList();
+            //passing value
+            return View(entries);
         }
 
     }
