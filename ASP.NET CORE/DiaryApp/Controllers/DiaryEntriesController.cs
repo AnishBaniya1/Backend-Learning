ea770beac40002a52676085a1cedc26080b6c2db
Anish Baniya <anishbaniya07@gmail.com>
2025-08-24 20:13:28 +0545

    learn: more in view section and also POST method

--- ASP.NET CORE/DiaryApp/Controllers/DiaryEntriesController.cs
+++ ASP.NET CORE/DiaryApp/Controllers/DiaryEntriesController.cs
@@ -22,5 +22,17 @@ namespace DiaryApp.Controllers
             return View(entries);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(DiaryEntry obj)
+        {
+            _db.DiaryEntries.Add(obj);//adds new diary entry to db
+            _db.SaveChanges();//saves the chnage4s to the db
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ASP.NET CORE/DiaryApp/obj/Debug/net9.0/DiaryApp.AssemblyInfo.cs
+++ ASP.NET CORE/DiaryApp/obj/Debug/net9.0/DiaryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd7f8b148f9ee7246cf37aa61e8023d9570ee45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a340c3b4c91a2220953aeb6f389d1fd03b0bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
