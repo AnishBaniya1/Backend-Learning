393dacb909429e09b6d1661467baf67188dbaa51
Anish Baniya <anishbaniya07@gmail.com>
2025-11-10 19:31:14 +0545

    feat: implement registration component

---
+++ ASP.NET CORE/Connectify/Frontend/src/app/services/auth-service.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { ApiResponse } from '../models/api-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  private baseUrl ='http://localhost:5159/api/Account';
+
+  httpClient= inject(HttpClient);
+
+  register(data: FormData): Observable<ApiResponse<string>>{
+    return this.httpClient.post<ApiResponse<string>>('${this.baseUrl}/register',data).pipe(tap((response)=>{
+      localStorage.setItem("token",response.data);
+    }))
+  }
+}
